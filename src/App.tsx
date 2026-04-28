7a7a180aa4ed7bf557c5e73113c286d4ef0fd0fe
abdulziz <aziz5522936989@gmail.com>
2026-04-28 15:23:50 +0300

    nav stype step one

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useState , useEffect} from 'react'
 import './App.css'
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 function App() {
 
+const [mode , serMode] = useState<string>(`Dark Mood`)
 
-  useEffect(()=>{
-    fetch("/data.json").then(
-      e => e.json()).then(e => console.log(e))
-  },[])
+  // useEffect(()=>{
+  //   fetch("/data.json").then(
+  //     e => e.json()).then(e => console.log(e))
+  // },[])
 
   return (
-    <>
-
-    </>
+    <div style={{width:`98dvw` , height:`98dvh` , backgroundColor:`#dddd`}} className="boody">
+        <div className="nav" style={{backgroundColor:`hsl(0, 0%, 99%)`}}>
+          <p>Where in the world?</p>
+          <p className="mood"> <DarkModeIcon />  { mode}</p>
+        </div>
+    </div>  
   )
 }
 
