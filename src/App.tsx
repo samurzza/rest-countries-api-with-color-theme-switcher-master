c741dd405dc8e971e71a571a21f8727553d153a4
abdulziz <aziz5522936989@gmail.com>
2026-04-29 15:57:36 +0300

    start Routaring

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import { useState , useEffect} from 'react'
+import { useState } from 'react'
 import './App.css'
-import DarkModeIcon from '@mui/icons-material/DarkMode';
+import Brightness4RoundedIcon from '@mui/icons-material/Brightness4Rounded'
+import Brightness4TwoToneIcon from '@mui/icons-material/Brightness4TwoTone';
+import { Route , Routes  } from 'react-router-dom';
+import AllCountries from "./country/AllCountries"
 
 function App() {
 
 const [mode , serMode] = useState<string>(`Dark Mood`)
+const [calssModd , setClassMood] = useState<string>(`left`)
 
-  // useEffect(()=>{
-  //   fetch("/data.json").then(
-  //     e => e.json()).then(e => console.log(e))
-  // },[])
+function Mood(){
+  if(mode === `Dark Mood`){
+  serMode(`Lite Mood`);
+  setClassMood(`rigth`);
+}else{
+  serMode(`Dark Mood`);
+  setClassMood(`left`);
+}
+}
 
-  return (
-    <div style={{width:`98dvw` , height:`98dvh` , backgroundColor:`#dddd`}} className="boody">
-        <div className="nav" style={{backgroundColor:`hsl(0, 0%, 99%)`}}>
-          <p>Where in the world?</p>
-          <p className="mood"> <DarkModeIcon />  { mode}</p>
-        </div>
-    </div>  
+return (
+<div style={{width:`98dvw` , height:`98dvh` , backgroundColor:`#dddd`}} className="boody">
+    <div className="nav" style={{backgroundColor:`hsl(0, 0%, 99%)` }}>
+      <p style={{fontWeight:`800` , fontSize:`x-large`}}>Where in the world?</p>
+      <p className="mood" style={{cursor:`pointer`}} onClick={Mood} > <span className={calssModd} >{mode === 'Dark Mood' ? <Brightness4TwoToneIcon /> : <Brightness4RoundedIcon /> }</span> { mode}</p>
+    </div>
+
+  <Routes>
+    <Route path='/' element={<AllCountries />}/>
+    <Route path='/contry' />
+  </Routes>
+</div>  
   )
 }
 
---
+++ src/country/AllCountries.tsx
@@ -0,0 +1,9 @@
+
+export default function AllCountries(){
+    return(
+        <>
+            <h1>all country</h1>
+            <h1>all country</h1>
+        </>
+    )
+}
