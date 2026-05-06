53b824d7b9649792f31acfde434e11103fde0e51
abdulziz <aziz5522936989@gmail.com>
2026-05-06 15:36:55 +0300

    search style don

--- src/country/Searsh.tsx
+++ src/country/Searsh.tsx
@@ -1,6 +1,8 @@
 // import {useState} from "react"
 import { useContext } from "react";
 import { searshContext } from "../context/searshContext";
+import SearchIcon from '@mui/icons-material/Search';
+
 
 
 export default function SearshCountyes(){
@@ -13,7 +15,10 @@ export default function SearshCountyes(){
 
     return(
         <>
-        <input type="search" value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
+        <div>
+            <SearchIcon  style={{transform: `translate(45px, 5px)`, color:`#a8a8a8`}} />
+        <input className="searchInput" type="search" value={search} placeholder="country name" onChange={(e)=>{setSearch(e.target.value)}} />
+        </div>
             
         <div style={{width:`50px` , backgroundColor:`red`}}>a</div>
         </>
