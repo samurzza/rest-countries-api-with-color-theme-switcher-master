9b9beb117b3640393aa2adca8d8d9342e887297c
abdulziz <aziz5522936989@gmail.com>
2026-05-08 15:05:41 +0300

    select disine is done

--- src/country/Searsh.tsx
+++ src/country/Searsh.tsx
@@ -1,5 +1,4 @@
-// import {useState} from "react"
-import { useContext } from "react";
+import { useContext , useState } from "react";
 import { searshContext } from "../context/searshContext";
 import SearchIcon from '@mui/icons-material/Search';
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
@@ -9,26 +8,58 @@ import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 export default function SearshCountyes(){
 
     const { search, setSearch } = useContext(searshContext);
+    const [regionCommit , setRegionCommit] = useState<string>(``)
+    const [selectHidin , setSelectHidin] = useState<string>(`none`)
+    const [calssModd , setClassMood] = useState<string>(`left`)
+
+
+    function clickFunction(name:string){
+            setRegionCommit(name)
+                if(selectHidin === `block`){
+                    setSelectHidin(`none`)
+                    setClassMood(`top`);
+
+                }else if(selectHidin === `none`){
+                    setSelectHidin(`block`)
+                    setClassMood(`bottum`);
+                }
+    }
+    
+    const styleStele = {
+        display:selectHidin
+    }
+
+
+    function chinge(){
+    if(selectHidin === `block`){
+        setSelectHidin(`none`)
+        setClassMood(`top`);
+
+    }else if(selectHidin === `none`){
+        setSelectHidin(`block`)
+        setClassMood(`bottum`);
+    }
+    }
 
     return(
-        <>
+        <   >
         <div>
             <SearchIcon  style={{transform: `translate(45px, 5px)`, color:`#a8a8a8`}} />
         <input className="searchInput" type="search" value={search} placeholder="country name" onChange={(e)=>{setSearch(e.target.value)}} />
         </div>
             
         <div className="region">
-            <div id="delectField">
-                <p>sealect Region</p>
-                <span><ArrowCircleDownIcon /></span>
+            <div onClick={chinge} id="delectField">
+                <p>{regionCommit || "Filter By Region"}</p>
+                <p><span  className={calssModd}><ArrowCircleDownIcon /></span></p>
             </div>  
 
-            <ul>
-                <li>Africa</li>
-                <li>America</li>
-                <li>Asia</li>
-                <li>Europe</li>
-                <li>Oceania</li>
+            <ul style={styleStele}>
+                <li onClick={() => clickFunction("Africa")}>Africa</li>
+                <li onClick={() => clickFunction("America")}>America</li>
+                <li onClick={() => clickFunction("Asia")}>Asia</li>
+                <li onClick={() => clickFunction("Europe")}>Europe</li>
+                <li onClick={() => clickFunction("Oceania")}>Oceania</li>
             </ul>    
         </div>
         </>
